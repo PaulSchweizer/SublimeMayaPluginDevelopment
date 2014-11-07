0e88ed7387fd19b1608cb5b71b9fa3c720b90d29
Paul Schweizer <paulschweizer@gmx.net>
2014-11-06 18:26:16 -0800

    updates

--- SublimeMayaPluginDevelopment.py
+++ SublimeMayaPluginDevelopment.py
@@ -1,7 +1,9 @@
-"""Created on 2014/09/10
-@brief: Sends the current maya plugin together with user defined code to maya.
-@author: Paul Schweizer
-@email: paulschweizer@gmx.net
+"""@date 2014/09/10
+@brief Plugin for maya plugin development
+@details Sends the current maya plugin together with user defined code
+         to maya.
+@author Paul Schweizer
+@email paulschweizer@gmx.net
 """
 
 import os
@@ -16,10 +18,31 @@ import sublime_plugin
 
 class TestMayaPluginCommand(sublime_plugin.TextCommand):
 
-    """Sends the current maya plugin together with user defined code to maya.
-    @todo: Modify the MayaSublime plugin in a way to be able to use it.
+    """Sends the current maya plugin together with user defined code to
+    maya.
+    @todo Modify the MayaSublime plugin in a way to be able to use it.
+    @todo Optimize some var retrievals
 
     """
+
+    def __init__(self, view):
+        """Initializes variables for the class."""
+        self._platform = self._get_platform()
+        self.view = view
+    # END def __init__
+
+    def _get_platform(self):
+        """Returns the current operating system
+        @todo implement for OSX
+
+        """
+        platform = sys.platform
+        if 'linux' in platform:
+            return 'linux'
+        elif 'windows' in platform:
+            return 'windows'
+        # END if
+    # END def _get_platform
 
     def run(self, edit):
         """The run method to execute the plugin testing.
@@ -38,12 +61,9 @@ class TestMayaPluginCommand(sublime_plugin.TextCommand):
 
         """
         name = os.path.basename(os.path.dirname(self.view.file_name()))
-        settings = self.settings().get('plugins')[name]
-        plugin_path = os.path.join(os.path.dirname(self.view.file_name()),
-                                   settings['platform'], 'Debug',
-                                   '%s.mll' % name)
-
-        custom_code = settings['code']
+        plugin_settings = self.settings.get('plugins')[name]
+        plugin_path = self._plugin_path(plugin_settings)
+        custom_code = plugin_settings['code']
         if isinstance(custom_code, list):
             custom_code = ''.join(custom_code)
         # END if
@@ -55,17 +75,35 @@ class TestMayaPluginCommand(sublime_plugin.TextCommand):
         self.run_plugin_command(command)
     # END def run
 
+    @property
     def settings(self):
         """Retrieves the settings.
-        @return: the settings
+        @return the settings
 
         """
-        return sublime.load_settings("TestMayaPlugin.sublime-settings")
+        return sublime.load_settings('SublimeMayaPluginDevelopment.sublime-settings')
     # END def settings
+
+    def _plugin_path(self, plugin_settings):
+        """Concatenates and returns the plugin path based on the
+        operating system.
+
+        """
+        name = os.path.basename(os.path.dirname(self.view.file_name()))
+        base = os.path.dirname(self.view.file_name())
+        file_ending = self.settings.get('file_endings')[self._platform]
+        if self._platform == 'linux':
+            return os.path.join(base, '%s.%s' % (name, file_ending))
+        elif self._platform == 'windows':
+            return os.path.join(base, plugin_settings['platform'], 'Debug',
+                                '%s.%s' % (name, file_ending))
+        # END if
+    # END def _plugin_path
 
     def run_plugin_command(self, command):
         """Runs the given command in maya to test the plugin.
-        @param command: the command to send to maya
+        @param command the command to send to maya
+        @todo properly format the string for the exec
 
         """
         PY_CMD_TEMPLATE = textwrap.dedent('''
@@ -88,16 +126,14 @@ class TestMayaPluginCommand(sublime_plugin.TextCommand):
         # Match single-line comments in MEL/Python
         RX_COMMENT = re.compile(r'^\s*(//|#)')
 
-        host = self.settings().get('host')
-        port = self.settings().get('port')
+        host = self.settings.get('host')
+        port = self.settings.get('port')
 
         print('Sending {0}:\n{1!r}\n...'.format('python', command[:200]))
 
         command = PY_CMD_TEMPLATE.format('exec',
                                          command.replace('\\r', '\\\\r'), '')
         c = None
-
-        print(command)
 
         try:
             c = Telnet(host, int(port), timeout=3)
@@ -109,7 +145,7 @@ class TestMayaPluginCommand(sublime_plugin.TextCommand):
                                   ' (%(host)s:%(port)s)):\n%(err)s' % locals())
             raise
         else:
-            time.sleep(.1)
+            time.sleep(0.1)
         finally:
             if c is not None:
                 c.close()
@@ -126,10 +162,7 @@ class UnloadMayaPluginCommand(TestMayaPluginCommand):
     def run(self, edit):
         """Builds the command to unload the current plugin in maya."""
         name = os.path.basename(os.path.dirname(self.view.file_name()))
-        settings = self.settings().get('plugins')[name]
-        plugin_path = os.path.join(os.path.dirname(self.view.file_name()),
-                                   settings['platform'], 'Debug',
-                                   '%s.mll' % name)
+        plugin_path = self._plugin_path(self.settings.get('plugins')[name])
         command = ('from maya import cmds, mel;'
                    'cmds.unloadPlugin("%(name)s", f=True);' % locals())
         self.run_plugin_command(command)
